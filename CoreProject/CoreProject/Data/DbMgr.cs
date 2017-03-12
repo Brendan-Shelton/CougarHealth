d44a1782fc1fa8c8948375ca70a2fd9574c6b364
mirhode <mirhode@siue.edu>
2017-03-12 00:55:32 -0600

    This commit works #1405 @1.08h
    I added and implemented the InsurancePlan and Service classes. In the DbMgr class I hard coded the GetPlans method that gives all the data given from Table II and Table III from the project spec

--- CoreProject/CoreProject/Data/DbMgr.cs
+++ CoreProject/CoreProject/Data/DbMgr.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoreProject.Data.Enrollee;
 
 namespace CoreProject.Data
 {
@@ -34,5 +35,312 @@ namespace CoreProject.Data
         /// </summary>
         /// <param name="enrollee"></param>
         public void SaveEnrollee(Enrollee.Enrollee enrollee) { }
+
+        public IEnumerable<Enrollee.InsurancePlan> GetPlans()
+        {
+            return new List<InsurancePlan>()
+            {
+                new InsurancePlan()
+                {
+                    Id = 1,
+                    Type = "Basic",
+                    APD = 250.0,
+                    PYMB = 250000.0,
+                    DependentFee = 20.0,
+                    PrimaryFee = 45.0,
+                    DependentChangeFee = 40.0,
+                    PrimaryChangeFee = 150.0,
+                    ServiceCosts = new[]
+                    {
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "inpatient",
+                            PercentCoverage = 0.9,
+                            RequiredCopayment = 400.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                2000,
+                                Service.MaxPayRate.Day
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Inpatient (Behavioral Health",
+                            PercentCoverage = 0.9,
+                            RequiredCopayment = 400.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                1500,
+                                Service.MaxPayRate.Day
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Emergency Room",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 250.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                1000,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Outpatient Surgery",
+                            PercentCoverage = 0.9,
+                            RequiredCopayment = 250,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                4000,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Diagnostic Lab and x-ray",
+                            PercentCoverage = .9,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                500,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Office Visit",
+                            PercentCoverage = .9,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                150,
+                                Service.MaxPayRate.PCY 
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Specialist Visit",
+                            PercentCoverage = .9,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY 
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Preventive Services",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                25,
+                                Service.MaxPayRate.PCY 
+                            )
+                        }, 
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Baby Services",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY
+                            )
+                        }, 
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Durable Medical Equipment",
+                            PercentCoverage = .8,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Nursing Facility",
+                            PercentCoverage = .9,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                250,
+                                Service.MaxPayRate.Day
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Physical Therapy",
+                            PercentCoverage = .9,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                100, Service.MaxPayRate.Session
+                            )
+                        }
+                    } // ServiceCosts
+                }, // Basic 
+                new InsurancePlan()
+                {
+                    Id = 2,
+                    Type = "Extended",
+                    APD = 0,
+                    PYMB = 1000000.0,
+                    DependentFee = 25.0,
+                    PrimaryFee = 65.0,
+                    DependentChangeFee = 20.0,
+                    PrimaryChangeFee = 50.0,
+                    ServiceCosts = new[]
+                    {
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Inpatient",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 300.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                2000,
+                                Service.MaxPayRate.Day
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Inpatient (Behavioral Health",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 300.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                1500,
+                                Service.MaxPayRate.Day
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Emergency Room",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 250.0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                1000,
+                                Service.MaxPayRate.PCY 
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Outpatient Surgery",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 250,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                4000,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Hospital",
+                            Name = "Diagnostic Lab and x-ray",
+                            PercentCoverage = 1.0,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                500,
+                                Service.MaxPayRate.PCY 
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Office Visit",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 20,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                150,
+                                Service.MaxPayRate.PCY 
+                            )
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Specialist Visit",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 30,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Preventive Services",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                25,
+                                Service.MaxPayRate.PCY 
+                            )
+
+                        }, 
+                        new Service
+                        {
+                            Category = "Physician",
+                            Name = "Baby Services",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY
+                            )
+
+                        }, 
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Durable Medical Equipment",
+                            PercentCoverage = .8,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                300,
+                                Service.MaxPayRate.PCY
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Nursing Facility",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 0,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                250,
+                                Service.MaxPayRate.Day
+                            )
+
+                        },
+                        new Service
+                        {
+                            Category = "Other",
+                            Name = "Physical Therapy",
+                            PercentCoverage = 1,
+                            RequiredCopayment = 30,
+                            InNetMax = new Tuple<double, Service.MaxPayRate>(
+                                100, Service.MaxPayRate.Session
+                            )
+                        }
+                    } // ServiceCosts
+                }
+            }; // List<InsurancePlan
+        }
     }
 }
