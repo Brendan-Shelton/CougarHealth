2b4aa8412e0ee2f84a0e1b76d756fbdb4fee602d
Brendan Shelton <brshelt@siue.edu>
2017-03-14 00:33:03 -0500

    This commit works #1418 @0.75h
    Added requests to DBMgr for policy number and enrollee. First checks for Policy, then Enrollee.

--- CoreProject/CoreProject/Controller/HSPControllers/BillController.cs
+++ CoreProject/CoreProject/Controller/HSPControllers/BillController.cs
@@ -4,20 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CoreProject.Data.Enrollee;
+using CoreProject.Data;
 
 namespace CoreProject.Controller.HSPControllers
 {
+    
+
     public class BillController
     {
-        public bool checkPolicy(String policy)
+        public DbMgr Mgr;
+        public IEnumerable<InsurancePlan> Plans;
+        public Type planType;
+        public Enrollee enrollee;
+        public EnrolleePlan enrolleePlan;
+
+        public BillController()
         {
-            return true;
+            this.Mgr = DbMgr.Instance;
+            this.Plans = Mgr.GetPlans();
+        }
+        
+        
+
+        public bool CheckPolicy(int policyID)
+        {
+            if (Mgr.GetPolicyByID(policyID) == null)
+            {
+                return false;
+            }
+            else
+            {
+                enrolleePlan = Mgr.GetPolicyByID(policyID);
+                return true;
+            }
+                
         }
 
-        public bool checkEnrollee(String enrollee)
+        public bool checkEnrollee(String name)
         {
-            return true;
+            String[] nameArr = new String[2];
+            nameArr = name.Split();
+            if (Mgr.GetEnrolleeByName(nameArr[0], nameArr[1]) == null)
+            {
+                return false;
+            }
+            else
+                enrollee = Mgr.GetEnrolleeByName(nameArr[0], nameArr[1]);
+                return true;
         }
+
+        
+        
+        
+
+        
+        
+
 
         public void HSPCalculate(InsurancePlan plan, String[] services, int[] charges)
         {
@@ -26,13 +68,13 @@ namespace CoreProject.Controller.HSPControllers
             
             for(int i = 0; i < services.Length; i++)
             {
-                //if(charges[i] > plan.getServiceCost(services[i]))
-                //{
-                //    adjustedCharge = plan.getService(services[i]);
-                //}
+                if(charges[i] > plan.getServiceCost(services[i]))
+                {
+                    double adjustedCharge = plan.getService(services[i]);
+                }
 
-                //int enrolleeCharge = (charges[i] * (1 - getService(services[i]).getPercentCoverage())) + getService(services[i]).getRequiredCopayment();
-                //int HSPCharge = (charges[i] * getService(services[i]).getPercentCoverage());
+                double enrolleeCharge = (charges[i] * (1 - getService(services[i]).getPercentCoverage())) + getService(services[i]).getRequiredCopayment();
+                double HSPCharge = (charges[i] * getService(services[i]).getPercentCoverage());
 
             }
 
@@ -43,10 +85,7 @@ namespace CoreProject.Controller.HSPControllers
 
         public void OHSPCalculate()
         {
-            //if(amount > allowedAmount)
-            //{
-            ///    enrolleeOverage = amount - allowedAmount;
-            //}
+            
         }
     }
 }
--- CoreProject/CoreProject/Data/Enrollee/Enrollee.cs
+++ CoreProject/CoreProject/Data/Enrollee/Enrollee.cs
@@ -8,5 +8,86 @@ namespace CoreProject.Data.Enrollee
 {
     public class Enrollee
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HomePhone { get; set; }
+        /// <summary>
+        /// optional for the enrollee to have
+        /// </summary>
+        public string MobilePhone { get; set; } = null;
+
+        /// <summary>
+        /// Place holder for the database 
+        /// </summary>
+        private static int idCount = 0;
+
+        public int Id { get; }
+        private string _pin;
+
+        /// <summary>
+        /// Basically a password for the enrollee that allows them to log in 
+        /// with their email.
+        /// TODO: hash the password when set
+        /// </summary>
+        public string Pin
+        {
+            get { return _pin; }
+            private set
+            {
+                if ( _pin == value )
+                    throw new ArgumentException("Pin can't be the same as previous");
+                _pin = value;
+            }
+        }
+
+        private string _ssn;
+        /// <summary>
+        /// Can only be set once because it is unique to the Enrollee
+        /// </summary>
+        public string SSN {
+            set
+            {
+                if ( _ssn != null )
+                    throw new ArgumentException("SSN can only be set once");
+
+                _ssn = value;
+            }
+            get { return _ssn; }
+        }
+        public string MailingAddr { get; set; }
+        /// <summary>
+        /// optional for the enrollee to have
+        /// </summary>
+        public string BillingAddr { get; set; } = null;
+
+        public string Email { get; set; }
+
+        public Enrollee()
+        {
+            this.Id = ++idCount;
+        }
+
+        public void changePIN(string newPin)
+        {
+            //TODO: hash pin 
+            this.Pin = newPin;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var that = (Enrollee) obj;
+            // optional members
+            var mobileEqual = that.MobilePhone?.Equals(this.MobilePhone) ?? true;
+            var billingEqual = that.BillingAddr?.Equals(this.BillingAddr) ?? true;
+
+            return that.Email.Equals(this.Email) &&
+                   that.FirstName.Equals(this.FirstName) &&
+                   that.LastName.Equals(this.LastName) &&
+                   billingEqual &&
+                   that.Pin.Equals(this.Pin) &&
+                   that.HomePhone.Equals(this.HomePhone) &&
+                   that.SSN.Equals(this.SSN) &&
+                   mobileEqual;
+        }
     }
 }
