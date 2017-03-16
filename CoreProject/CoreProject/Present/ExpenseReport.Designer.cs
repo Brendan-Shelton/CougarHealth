cd96f017f01b19cb650dfcc6fcc35fc22e7add39
zauer <zauer@siue.edu>
2017-03-15 19:35:43 -0500

    Changes the color of the Reports

--- CoreProject/CoreProject/Present/ExpenseReport.Designer.cs
+++ CoreProject/CoreProject/Present/ExpenseReport.Designer.cs
@@ -249,8 +249,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(686, 446);
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(659, 416);
             this.Controls.Add(this.ExtendedPercNumLabel);
             this.Controls.Add(this.ExtendedPercentLabel);
             this.Controls.Add(this.BasicPercNumLabel);
--- CoreProject/CoreProject/Present/MonthlyReport.Designer.cs
+++ CoreProject/CoreProject/Present/MonthlyReport.Designer.cs
@@ -539,6 +539,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(667, 732);
             this.Controls.Add(this.OHSPOtherNumBills);
             this.Controls.Add(this.OHSPPhysicianNumBills);
