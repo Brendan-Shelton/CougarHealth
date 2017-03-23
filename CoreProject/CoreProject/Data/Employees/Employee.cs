b4cfd52f8d17075b0fe7fcd6739fa142debe2716
Michael Rhodes <mirhode@siue.edu>
2017-03-22 22:23:33 -0500

    This commit works #2279 @0.7h

---
+++ CoreProject/CoreProject/Data/Employees/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProject.Data.Employees
+{
+    /// <summary>
+    /// An employee from cougar health 
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// What the employee can do 
+        /// </summary>
+        public Permission Permission { get; set; }
+        /// <summary>
+        /// The username used to identify the employee stored in the database 
+        /// </summary>
+        public string UserName { get; set; }
+
+        private string _password;
+        /// <summary>
+        /// The hashed password stored in the database 
+        /// </summary>
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = this.Passwordify(value);
+            }
+        }
+
+        /// <summary>
+        /// hash and salt the password of the employee 
+        /// </summary>
+        /// <param name="plainPass">plain text password</param>
+        /// <returns>hashed password</returns>
+        private string Passwordify(string plainPass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CoreProject/CoreProject/Data/Employees/Permission.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProject.Data.Employees
+{
+    /// <summary>
+    /// The permission of an employee allows to only be able to do certain 
+    /// things
+    /// PlanAdmin - can access the CougarCostsController
+    /// EnrolleeSupport - can access EnrolleeSupportController
+    /// HSPSupport - can access HSPSupportController
+    /// Accountant - can access the RangeReportController and MonthReportController
+    /// Manager - can do anything plus ManageEmployeeController and CreateEmployeeController
+    /// </summary>
+    public enum Permission
+    {
+        PlanAdmin,
+        EnrolleeSupport,
+        HSPSupport,
+        Accountant,
+        Manager
+    }
+}
